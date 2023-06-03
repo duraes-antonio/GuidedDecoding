0608b4f038ccc57e9e40384238b3562ca42d1362
duraes-antonio <garotoseis@gmail.com>
2023-06-03 16:43:00 -0300

    FEAT: Center seed definition for all libs

---
+++ reproducibility.py
@@ -0,0 +1,28 @@
+import random
+
+import numpy
+import torch
+
+
+def set_all_lib_seed(seed: int, loader=None) -> None:
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)
+    torch.use_deterministic_algorithms(True)
+    torch.cuda.manual_seed(seed)
+    random.seed(seed)
+    numpy.random.seed(seed)
+
+    try:
+        loader.sampler.generator.manual_seed(seed)
+    except AttributeError:
+        pass
+
+    return None
+
+
+def set_seed_worker(worker_id) -> None:
+    worker_seed = torch.initial_seed() % 2 ** 32
+    numpy.random.seed(worker_seed)
+    random.seed(worker_seed)
+    return None
