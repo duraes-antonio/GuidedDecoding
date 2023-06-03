cebedf4b532ecbb6a9b6f2b02586fc78cfda51bb
duraes-antonio <garotoseis@gmail.com>
2023-06-03 15:58:41 -0300

    REFACTOR: Add unet 3+ files, update loader

--- model/loader.py
+++ model/loader.py
@@ -1,31 +1,28 @@
+import random
+
+import numpy
 import torch
+import torch.backends.cudnn as cudnn
 
-from model.AttentionUnet.attention_unet import U_Net, AttU_Net
-from model.GuideDepth import GuideDepth
+from config import SEED
+from model.unet_3_plus.unet_3_plus import UNet_3Plus
 
 
-def load_model(model_name, weights_pth):
-    model = model_builder(model_name)
+def load_model(load_weights=False):
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    random.seed(SEED)
+    numpy.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
 
-    if weights_pth is not None:
-        state_dict = torch.load(weights_pth, map_location='cpu')
-        model.load_state_dict(state_dict)
+    model = UNet_3Plus()
+    # model = MTUNet(1)
+
+    if load_weights:
+        model.load_state_dict(torch.load('results/best_model.pth'))
+        # model.load_state_dict(torch.load('results/best_model.pth'))
 
     return model
 
 
-def model_builder(model_name):
-    if model_name == 'GuideDepth':
-        return GuideDepth(True)
-
-    if model_name == 'GuideDepth-S':
-        return GuideDepth(True, up_features=[32, 8, 4], inner_features=[32, 8, 4])
-
-    if model_name == 'U_Net':
-        return U_Net(img_ch=3, output_ch=1)
-
-    elif model_name == 'AttU_Net':
-        return AttU_Net(img_ch=3, output_ch=1)
-
-    print("Invalid model")
-    exit(0)
