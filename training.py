0bfaffab395c9ceff9ec0a835dc1a4e5df6b4b55
duraes-antonio <garotoseis@gmail.com>
2023-06-03 19:10:22 -0300

    FIX: remove code for validation stage

--- training.py
+++ training.py
@@ -3,9 +3,9 @@ import time
 
 import torch
 import torch.optim as optim
-from data import datasets
 from tqdm import tqdm
 
+from dataset import datasets
 from losses import Depth_Loss
 from metrics import AverageMeter, Result
 from model import loader
@@ -35,10 +35,9 @@ class Trainer():
         print('Maximum Depth of Dataset: {}'.format(self.maxDepth))
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-        self.model = loader.load_model(args.model,
-                                       args.weights_path)
+        # Initialize the dataset and the dataloader
+        self.model = loader.load_model()
         self.model.to(self.device)
-
         self.train_loader = datasets.get_dataloader(args.dataset,
                                                     path=args.data_path,
                                                     split='train',
@@ -46,19 +45,10 @@ class Trainer():
                                                     batch_size=args.batch_size,
                                                     resolution=args.resolution,
                                                     workers=args.num_workers)
-        self.val_loader = datasets.get_dataloader(args.dataset,
-                                                  path=args.data_path.replace('_train', '_test'),
-                                                  split='val',
-                                                  augmentation=args.eval_mode,
-                                                  batch_size=args.batch_size,
-                                                  resolution=args.resolution,
-                                                  workers=args.num_workers)
-
-        self.optimizer = optim.Adam(self.model.parameters(),
-                                    args.learning_rate)
-        self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer,
-                                                      args.scheduler_step_size,
-                                                      gamma=0.1)
+        self.optimizer = optim.Adam(self.model.parameters(), args.learning_rate)
+        self.lr_scheduler = optim.lr_scheduler.StepLR(
+            self.optimizer, args.scheduler_step_size, gamma=0.1
+        )
 
         if args.eval_mode == 'alhashim':
             self.loss_func = Depth_Loss(0.1, 1, 1, maxDepth=self.maxDepth)
@@ -72,15 +62,11 @@ class Trainer():
     def train(self):
         torch.cuda.empty_cache()
         self.start_time = time.time()
+
         for self.epoch in range(self.epoch, self.max_epochs):
             current_time = time.strftime('%H:%M', time.localtime())
             print('{} - Epoch {}'.format(current_time, self.epoch))
-
             self.train_loop()
-
-            if self.val_loader is not None:
-                self.val_loop()
-
             self.save_checkpoint()
 
         self.save_model()
@@ -88,68 +74,43 @@ class Trainer():
     def train_loop(self):
         self.model.train()
         accumulated_loss = 0.0
+        average_meter = AverageMeter()
 
         for i, data in enumerate(tqdm(self.train_loader)):
+            t0 = time.time()
             image, gt = self.unpack_and_move(data)
             self.optimizer.zero_grad()
+            data_time = time.time() - t0
 
+            t0 = time.time()
             prediction = self.model(image)
+            gpu_time = time.time() - t0
 
             loss_value = self.loss_func(prediction, gt)
             loss_value.backward()
             self.optimizer.step()
 
             accumulated_loss += loss_value.item()
+            result = Result()
+            result.evaluate(prediction.data, gt.data)
+            average_meter.update(result, gpu_time, data_time, image.size(0))
 
         # Report
-        current_time = time.strftime('%H:%M', time.localtime())
+        time.strftime('%H:%M', time.localtime())
         average_loss = accumulated_loss / (len(self.train_loader.dataset) + 1)
-        print('{} - Average Training Loss: {:3.4f}'.format(current_time, average_loss))
-
-    def val_loop(self):
-        torch.cuda.empty_cache()
-        self.model.eval()
-        accumulated_loss = 0.0
-        average_meter = AverageMeter()
-
-        with torch.no_grad():
-            for i, data in enumerate(self.val_loader):
-                t0 = time.time()
-                image, gt = self.unpack_and_move(data)
-                data_time = time.time() - t0
-
-                t0 = time.time()
-                inv_prediction = self.model(image)
-                prediction = self.inverse_depth_norm(inv_prediction)
-                gpu_time = time.time() - t0
-
-                if self.debug and i == 0:
-                    self.show_images(image, gt, prediction)
-
-                loss_value = self.loss_func(inv_prediction, self.depth_norm(gt))
-                accumulated_loss += loss_value.item()
-
-                result = Result()
-                result.evaluate(prediction.data, gt.data)
-                average_meter.update(result, gpu_time, data_time, image.size(0))
-
-        # Report
         avg = average_meter.average()
-        current_time = time.strftime('%H:%M', time.localtime())
-        average_loss = accumulated_loss / (len(self.val_loader.dataset) + 1)
-        self.val_losses.append(average_loss)
-        print('{} - Average Validation Loss: {:3.4f}'.format(current_time, average_loss))
-
-        print('\n*\n'
-              'RMSE={average.rmse:.3f}\n'
-              'MAE={average.mae:.3f}\n'
-              'Delta1={average.delta1:.3f}\n'
-              'Delta2={average.delta2:.3f}\n'
-              'Delta3={average.delta3:.3f}\n'
-              'REL={average.absrel:.3f}\n'
-              'Lg10={average.lg10:.3f}\n'
-              't_GPU={time:.3f}\n'.format(
-            average=avg, time=avg.gpu_time))
+        print(
+            '\n*\n'
+            'Average Training Loss: {average_loss:3.4f}\n'
+            'RMSE={average.rmse:.3f}\n'
+            'MAE={average.mae:.3f}\n'
+            'Delta1={average.delta1:.3f}\n'
+            'Delta2={average.delta2:.3f}\n'
+            'Delta3={average.delta3:.3f}\n'
+            'REL={average.absrel:.3f}\n'
+            'Lg10={average.lg10:.3f}\n'
+            't_GPU={time:.3f}\n'.format(average=avg, time=avg.gpu_time, average_loss=average_loss)
+        )
 
     def load_checkpoint(self, checkpoint_path):
         checkpoint = torch.load(checkpoint_path,
@@ -161,8 +122,7 @@ class Trainer():
 
     def save_checkpoint(self):
         # Save checkpoint for training
-        checkpoint_dir = os.path.join(self.checkpoint_pth,
-                                      'checkpoint_{}.pth'.format(self.epoch))
+        checkpoint_dir = os.path.join(self.checkpoint_pth, 'checkpoint_{}.pth'.format(self.epoch))
         torch.save({
             'epoch': self.epoch + 1,
             'val_losses': self.val_losses,
@@ -174,11 +134,8 @@ class Trainer():
         print('{} - Model saved'.format(current_time))
 
     def save_model(self):
-        best_checkpoint_pth = os.path.join(self.checkpoint_pth,
-                                           f'checkpoint_{self.max_epochs - 1}.pth')
-        best_model_pth = os.path.join(self.results_pth,
-                                      'best_model.pth')
-
+        best_checkpoint_pth = os.path.join(self.checkpoint_pth, f'checkpoint_{self.max_epochs - 1}.pth')
+        best_model_pth = os.path.join(self.results_pth, 'best_model.pth')
         checkpoint = torch.load(best_checkpoint_pth)
         torch.save(checkpoint['model'], best_model_pth)
         print('Model saved.')
