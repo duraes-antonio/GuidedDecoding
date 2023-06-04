60a449f5a84254dd180c0ba47bbfa61b670558bb
duraes-antonio <garotoseis@gmail.com>
2023-06-04 15:20:10 -0300

    REFACTOR: isolate function to print results

---
+++ util/log.py
@@ -0,0 +1,15 @@
+from metrics import Result
+
+
+def print_metrics(results: Result) -> None:
+    print(
+        '\n*\n'
+        'RMSE={average.rmse:.3f}\n'
+        'MAE={average.mae:.3f}\n'
+        'Delta1={average.delta1:.3f}\n'
+        'Delta2={average.delta2:.3f}\n'
+        'Delta3={average.delta3:.3f}\n'
+        'REL={average.absrel:.3f}\n'
+        'Lg10={average.lg10:.3f}\n'
+        't_GPU={time:.3f}\n'.format(average=results, time=results.gpu_time)
+    )
