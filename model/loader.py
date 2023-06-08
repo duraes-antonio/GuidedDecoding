5fc368fb316d828be863693a0ed0eab811042c1b
duraes-antonio <garotoseis@gmail.com>
2023-06-07 21:39:54 -0300

    FEAT: allow pass custom wights path to load

--- model/loader.py
+++ model/loader.py
@@ -5,7 +5,7 @@ from model.unet_3_plus.unet_3_plus import UNet_3Plus
 from options.model import Models
 
 
-def load_model(model: Models, load_weights=False):
+def load_model(model: Models, load_weights=False, path_weights='results/best_model.pth'):
     if model == Models.UNet3Plus:
         model = UNet_3Plus()
 
@@ -13,6 +13,6 @@ def load_model(model: Models, load_weights=False):
         model = MTUNet(1)
 
     if load_weights:
-        model.load_state_dict(torch.load('results/best_model.pth'))
+        model.load_state_dict(torch.load(path_weights))
 
     return model
