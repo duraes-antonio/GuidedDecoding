4c61a8e9b98fc22fedf60ec1fc53299e3655cff1
Antônio Carlos Durães da Silva <37306576+duraes-antonio@users.noreply.github.com>
2023-07-10 00:11:03 -0300

    FEAT: Include TransFuse in model.py

--- options/model.py
+++ options/model.py
@@ -7,6 +7,7 @@ class Models(Enum):
     NestedUnet = 'nested-unet'
     UNet3Plus = 'unet3+'
     TransUnet = 'trans-unet'
+    TransFuse = 'trans-fuse'
     TransUnetPlus = 'trans-unet+'
     TransUnetDecoder3Skips = 'trans-unet-skips3'
     TransUnetPlusPlus = 'trans-unet++'
