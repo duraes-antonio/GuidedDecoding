ee989a4f4ee20b01bb912d844e41f3bfd2ee9a4b
Antônio Carlos Durães da Silva <37306576+duraes-antonio@users.noreply.github.com>
2023-07-10 00:47:19 -0300

    Update dataset_resolution.py

--- options/dataset_resolution.py
+++ options/dataset_resolution.py
@@ -6,6 +6,7 @@ class Resolutions(Enum):
     Mini = 'mini'
     Half = 'half'
     Full = 'full'
+    TransFuseMini = 'trans-fuse_mini'
 
     def __str__(self):
         return self.value
@@ -14,5 +15,6 @@ class Resolutions(Enum):
 shape_by_resolution: Dict[Resolutions, Tuple[int, int]] = {
     Resolutions.Full: (480, 640),
     Resolutions.Half: (240, 320),
-    Resolutions.Mini: (224, 224)
+    Resolutions.Mini: (224, 224),
+    Resolutions.TransFuseMini: (192, 256),
 }
