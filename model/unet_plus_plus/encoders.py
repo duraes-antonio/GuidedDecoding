39af6357b3488b827788522f129ba47ec519fbb2
duraes-antonio <garotoseis@gmail.com>
2023-08-20 23:05:28 -0300

    FEAT: Add custom resnet encoder

--- model/unet_plus_plus/encoders.py
+++ model/unet_plus_plus/encoders.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 from torchvision import models
-from torchvision.models import VGG16_Weights
 
 
 def count_parameters(model: nn.Module) -> int:
@@ -30,7 +29,7 @@ class LuizMobileNetV2Encoder(nn.Module):
 class LuizVGG16Encoder(nn.Module):
     def __init__(self):
         super(LuizVGG16Encoder, self).__init__()
-        self.encoder = models.vgg16(weights=VGG16_Weights.DEFAULT)
+        self.encoder = models.vgg16()
         freeze = True
 
         for param in self.encoder.parameters():
@@ -46,14 +45,22 @@ class LuizVGG16Encoder(nn.Module):
 class LuizResNetEncoder(nn.Module):
     def __init__(self):
         super(LuizResNetEncoder, self).__init__()
-        self.encoder = models.resnet152(pretrained=True)
-        freeze = True
-
-        for param in self.encoder.parameters():
-            param.requires_grad = not freeze
+        resnet101 = models.resnet101(pretrained=True)
+        encoder_layers = [
+            resnet101.conv1,
+            resnet101.bn1,
+            resnet101.relu,
+            resnet101.maxpool,
+            resnet101.layer1,
+            resnet101.layer2,
+            resnet101.layer3,
+            resnet101.layer4
+        ]
+        self.encoder = nn.Sequential(*encoder_layers)
 
     def forward(self, x):
-        features = [x]
-        for _, v in self.encoder.features._modules.items():
-            features.append(v(features[-1]))
-        return features
+        enc_features = [x]
+        for layer in self.encoder:
+            x = layer(x)
+            enc_features.append(x)
+        return enc_features
